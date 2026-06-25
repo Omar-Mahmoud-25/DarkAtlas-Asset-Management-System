2e16b8e9816336e34fd288aa6a6d163c8d83414f
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2026-06-25 09:17:12 +0300

    added db-health

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlmodel import select
+from sqlalchemy.exc import OperationalError
+from src.core import get_db_session
 
 app = FastAPI()
 
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
+
+@app.get("/db-health")
+async def db_health_check(session = Depends(get_db_session)):
+    try:
+        statement = select(1)
+        return {"status": f"healthy {list(session.exec(statement).all())}"}
+    except OperationalError:
+        return {"status": "unhealthy"}
